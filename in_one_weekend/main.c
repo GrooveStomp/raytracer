2ac55e939ce7684dba8a2e9ac2e69a8f1899efeb
GrooveStomp <aaron.oman@gmail.com>
2016-08-27 16:50:31 -0700

    WIP - need to link with math, write ray3 interface.

--- in_one_weekend/main.c
+++ in_one_weekend/main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "vec3.h"
+#include "ray3.h"
+
+vec3 ComputeColor(ray3 *Ray)
+{
+        vec3 UnitDirection = Vec3Unit(RayDirection(Ray));
+        float T = 0.5 * (UnitDirection.Y + 1.0);
+
+        vec3 A = { 1.0, 1.0, 1.0 };;
+//        Vec3Init(&A, 1.0, 1.0, 1.0);
+        A = Vec3ScalarMultiply(A, (1.0 - T));
+
+        vec3 B = { 0.5, 0.7, 1.0 };;
+//        Vec3Init(&B, 0.5, 0.7, 1.0);
+        B = Vec3ScalarMultiply(B, T);
+
+        vec3 Result = Vec3VectorAdd(A, B);
+        return(Result);
+}
+
 int
 main(int ArgCount, char **Arguments)
 {
@@ -9,17 +29,31 @@ main(int ArgCount, char **Arguments)
 
         printf("P3\n%i %i\n255\n", X, Y);
 
+        vec3 LowerLeftCorner = { -2.0, -1.0, -1.0 }; //Vec3Init((vec3 *)alloca(sizeof(vec3), -2.0, -1.0, -1.0));
+        vec3 Horizontal = { 4.0, 0.0, 0.0 }; //Vec3Init((vec3 *)alloca(sizeof(vec3), 4.0, 0.0, 0.0));
+        vec3 Vertical = { 0.0, 2.0, 0.0 }; //Vec3Init((vec3 *)alloca(sizeof(vec3), 0.0, 2.0, 0.0));
+        vec3 Origin = { 0.0, 0.0, 0.0 }; //Vec3Init((vec3 *)alloca(sizeof(vec3)), 0.0, 0.0, 0.0);
+
         for(int J = Y-1; J >= 0; J--)
         {
                 for(int I = 0; I < X; I++)
                 {
-                        float R = (float)I / (float)X;
-                        float G = (float)J / (float)Y;
-                        float B = 0.2;
+                        float U = (float)I / (float)X;
+                        float V = (float)J / (float)Y;
 
-                        int RInt = (int)(255.99 * R);
-                        int GInt = (int)(255.99 * G);
-                        int BInt = (int)(255.99 * B);
+                        vec3 UHorizontal = Vec3ScalarMultiply(Horizontal, U);
+                        vec3 VVertical = Vec3ScalarMultiply(Vertical, V);
+
+                        vec3 Sum;
+                        Sum = Vec3Add(LowerLeftCorner, UHorizontal);
+                        Sum = Vec3Add(Sum, VVertical);
+                        ray3 Ray = { Origin, Sum };
+
+                        vec3 Color = ComputeColor(Sum);
+
+                        int RInt = (int)(255.99 * Color.R);
+                        int GInt = (int)(255.99 * Color.G);
+                        int BInt = (int)(255.99 * Color.B);
 
                         printf("%i %i %i\n", RInt, GInt, BInt);
                 }
---
+++ in_one_weekend/vec3.h
@@ -0,0 +1,67 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+#include <math.h>
+
+typedef struct
+{
+        union
+        {
+                struct
+                {
+                        float X;
+                        float Y;
+                        float Z;
+                };
+                struct
+                {
+                        float R;
+                        float G;
+                        float B;
+                };
+        }
+} vec3;
+
+vec3 Vec3Add(vec3 A, vec3 B)
+{
+        vec3 Result;
+        Result.X = A.X + B.X;
+        Result.Y = A.Y + B.Y;
+        Result.Z = A.Z + B.Z;
+        return(Result);
+}
+
+vec3 Vec3ScalarMultiply(vec3 Vector, float Scalar)
+{
+        vec3 Result;
+        Result.X = Vector.X * Scalar;
+        Result.Y = Vector.Y * Scalar;
+        Result.Z = Vector.Z * Scalar;
+        return(Result);
+}
+
+vec3 Vec3ScalarDivide(vec3 Vector, float Scalar)
+{
+        vec3 Result;
+        Result.X = Vector.X / Scalar;
+        Result.Y = Vector.Y / Scalar;
+        Result.Z = Vector.Z / Scalar;
+        return(Result);
+}
+
+vec3 Vec3Length(vec3 Vector)
+{
+        float X = Vector.X * Vector.X;
+        float Y = Vector.Y * Vector.Y;
+        float Z = Vector.Z * Vector.Z;
+        float Result = sqrt(X + Y + Z);
+        return(Result);
+}
+
+vec3 Vec3Unit(vec3 Vector)
+{
+        vec3 Result = Vec3ScalarDivide(Vector, Vec3Length(Vector));
+        return(Result);
+}
+
+#endif /* VEC3_H */
